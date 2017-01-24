8c27a0ef011f33ffbf0fd7dd55c1d308e2200369
saurabh <saurabh.shendye@gmail.com>
2017-01-24 08:21:40 -0700

    Constructor methods for Registry and Messaging node are created

--- src/cs455/overlay/node.java
+++
@@ -1,7 +0,0 @@
-package cs455.overlay;
-
-/**
- * Created by saurabh on 1/23/17.
- */
-public class node {
-}
---
+++ src/cs455/overlay/node/Node.java
@@ -0,0 +1,27 @@
+package cs455.overlay.node;
+
+/*
+ * Created by Saurabh Shendye on 1/23/17.
+ */
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+
+public class Node
+{
+    public Node(int port) throws IOException
+    {
+        ServerSocket server = new ServerSocket(port, 5);
+    }
+
+    public Node() throws IOException
+    {
+        // This will create a socket and will not bind to any of the port
+        ServerSocket server = new ServerSocket();
+        // This will bind the socket to any port with capability of handling 5 connections at a time
+        server.bind(null, 5);
+    }
+    
+}
+
