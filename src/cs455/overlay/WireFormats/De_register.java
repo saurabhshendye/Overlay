04e0b09d5f0faf32c06482171cb5f9710322b602
saurabh <saurabh.shendye@gmail.com>
2017-01-27 19:55:21 -0700

    Wrote Marshaling method in reg_request class. Yet to be tested

--- src/cs455/overlay/WireFormats/De_register.java
+++ src/cs455/overlay/WireFormats/De_register.java
@@ -1,7 +1,7 @@
-package cs455.overlay.WireFormats;
-
-/**
+/*
  * Created by saurabh on 1/27/17.
  */
+package cs455.overlay.WireFormats;
+
 public class De_register {
 }
--- src/cs455/overlay/WireFormats/Register_request.java
+++ src/cs455/overlay/WireFormats/Register_request.java
@@ -4,18 +4,40 @@
 
 package cs455.overlay.WireFormats;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 
 public class Register_request
 {
     private int port;
-    private InetAddress IP;
+    private String IP;
     private int type = 0;
 
-    public Register_request(int port, InetAddress IP)
+    public Register_request(int port, String IP)
     {
         this.IP = IP;
         this.port = port;
     }
 
+    public byte[] getBytes() throws IOException
+    {
+        byte[] marshaled = null;
+        ByteArrayOutputStream baopstream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(baopstream);
+        dout.writeInt(type);
+        byte[] b = this.getBytes();
+        int Len = b.length;
+        dout.writeInt(Len);
+        dout.write(b);
+        dout.flush();
+
+        marshaled = baopstream.toByteArray();
+
+        baopstream.close();
+        dout.close();
+
+        return marshaled;
+    }
 }
