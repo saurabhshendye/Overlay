ebc92570c8870a8462a8c9abc986b836741f356a
saurabhshendye <Saurabh.shendye@gmail.com>
2017-02-09 11:57:59 -0700

    Created calculate distance method

--- src/cs455/overlay/Dijkstra/Shortest_Path.java
+++ src/cs455/overlay/Dijkstra/Shortest_Path.java
@@ -4,7 +4,23 @@
 
 package cs455.overlay.Dijkstra;
 
+import java.util.ArrayList;
+
 public class Shortest_Path
 {
+    private int [][] weight_graph;
+    private ArrayList<String> Nodes;
+    private String source;
 
+    public Shortest_Path(int [][] weight_graph, ArrayList<String> Nodes, String source )
+    {
+        this.Nodes = Nodes;
+        this.weight_graph = weight_graph;
+        this.source = source;
+    }
+
+    public void calculatie_distances()
+    {
+
+    }
 }
