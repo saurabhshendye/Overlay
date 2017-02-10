0a57d040afcd4ece444cad65cc64949fdb57db7e
saurabhshendye <Saurabh.shendye@gmail.com>
2017-02-09 22:47:25 -0700

    get_successor(current_node)

--- src/cs455/overlay/Dijkstra/Shortest_Path.java
+++ src/cs455/overlay/Dijkstra/Shortest_Path.java
@@ -107,7 +107,7 @@ public class Shortest_Path
                     }
                     else
                     {
-                        Add_successor(get_successor(Nodes.get(i)), i);
+                        Add_successor(get_successor(current_node), i);
                     }
 //                    System.out.println("Added:  " + current_node + " as predecessor for: " + Nodes.get(i));
                     System.out.println();
