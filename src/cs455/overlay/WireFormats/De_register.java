00f7f8a9c4d99025cac945a60d3b91f5ee624c5a
saurabhshendye <Saurabh.shendye@gmail.com>
2017-02-11 14:46:38 -0700

    De-register class created

--- src/cs455/overlay/WireFormats/De_register.java
+++ src/cs455/overlay/WireFormats/De_register.java
@@ -3,5 +3,35 @@
  */
 package cs455.overlay.WireFormats;
 
-public class De_register {
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class De_register
+{
+    private int port;
+    private String IP;
+
+    public De_register(String IP, int port)
+    {
+        this.IP = IP;
+        this.port = port;
+    }
+
+    public byte [] getByteArray() throws IOException
+    {
+        // This method creates a byte array which needs to be written onto the socket
+        ByteArrayOutputStream baopstream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baopstream));
+
+
+        byte[] marshaled = baopstream.toByteArray();
+
+        baopstream.close();
+        dout.close();
+
+        return marshaled;
+    }
+
 }
